bbe797ba5126ff40d8e97acc2b40bb0a6072b5b3
Valerio Innocenti Sedili <valerio.innocenti.ext@yoox.com>
2019-04-06 23:40:35 +0200

    fix build issue on netlify

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,13 +22,6 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/content/assets`,
-        name: `assets`,
-      },
-    },
-    {
       resolve: `gatsby-plugin-typography`,
       options: {
         pathToConfigModule: `src/utils/typography`,
