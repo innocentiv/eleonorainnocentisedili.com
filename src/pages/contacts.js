f94bfb1d50126d52170d19509b49c94009fd520e
Valerio Innocenti Sedili <vinnocenti@outlook.it>
2019-05-01 17:46:01 +0200

    feat: add contact form

--- src/pages/contacts.js
+++ src/pages/contacts.js
@@ -1,16 +1,88 @@
-import React from "react"
-import { Link } from "gatsby"
+import React, { useState } from "react"
+import { Link, navigate } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const ContactsPage = () => (
-  <Layout>
-    <SEO title="Contatti" />
-    <h1>Contatti</h1>
-    <p>WIP - Form Contatti</p>
-    <Link to="/">Vai all'homepage</Link>
-  </Layout>
-)
+const encode = data =>
+  Object.keys(data)
+    .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&")
+
+const ContactsPage = () => {
+  const [values, setValues] = useState({})
+
+  const handleSubmit = event => {
+    event.preventDefault()
+    const form = event.target
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({
+        "form-name": form.getAttribute("name"),
+        ...values,
+      }),
+    })
+      .then(() => navigate(form.getAttribute("action")))
+      .catch(error => alert(error))
+  }
+
+  const handleChange = event => {
+    event.persist()
+    setValues(values => ({
+      ...values,
+      [event.target.name]: event.target.value,
+    }))
+  }
+
+  return (
+    <Layout>
+      <SEO title="Contatti" />
+      <h1>Contatti</h1>
+      <form
+        name="contact"
+        method="post"
+        action="/thanks/"
+        data-netlify="true"
+        data-netlify-honeypot="bot-field"
+        onSubmit={handleSubmit}
+      >
+        {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
+        <input type="hidden" name="form-name" value="contact" />
+        <p hidden>
+          <label>
+            Don’t fill this out:{" "}
+            <input name="bot-field" onChange={handleChange} />
+          </label>
+        </p>
+        <p>
+          <label>
+            Your name:
+            <br />
+            <input type="text" name="name" onChange={handleChange} />
+          </label>
+        </p>
+        <p>
+          <label>
+            Your email:
+            <br />
+            <input type="email" name="email" onChange={handleChange} />
+          </label>
+        </p>
+        <p>
+          <label>
+            Message:
+            <br />
+            <textarea name="message" onChange={handleChange} />
+          </label>
+        </p>
+        <p>
+          <button type="submit">Send</button>
+        </p>
+      </form>
+      <Link to="/">Vai all'homepage</Link>
+    </Layout>
+  )
+}
 
 export default ContactsPage
